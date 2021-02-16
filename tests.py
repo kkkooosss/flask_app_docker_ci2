ea0e2a4cca47956bbdcc7b961dd135937862ea85
kkkooosss <47805749+kkkooosss@users.noreply.github.com>
2021-02-16 17:00:16 +0200

    Update tests.py

--- tests.py
+++ tests.py
@@ -16,7 +16,7 @@ class TestCase(unittest.TestCase):
         self.assertEqual(response.status_code, 200)
 
     def test_matrix(self):
-        response = self.app.get('/matrix/123n459,789', follow_redirects=True)
+        response = self.app.get('/matrix/123n459n789', follow_redirects=True)
         # Change when the matrix route is fixed and returning the actual matrix
         self.assertEqual(response.status_code, 500)
 
