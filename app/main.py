70e0ae5c4bb69de967d5b74e064e1e1a6a8c4061
kkkooosss <kkacar@mail.ru>
2021-02-16 16:33:52 +0200

    updated main.py

--- app/main.py
+++ app/main.py
@@ -9,7 +9,7 @@ import json
 app = Flask(__name__)
 
 
-@app.routee("/", methods=['GET'])
+@app.route("/", methods=['GET'])
 def index():
     return pretty_json({
         "resources": {
@@ -24,20 +24,39 @@ def index():
 
 @app.route("/matrix/<matrix>", methods=['GET'])
 def matrix(matrix):
+    b = matrix.split('n')
+    m = '\n'.join(b)
+    return m
+
     # TODO: return matrix, each row in a new line
-    pass
 
 
-@app.route("/matrix/<matrix>/<column_number>", methods=['GET'])
+@app.route("/columns/matrix/<matrix>/<column_number>", methods=['GET'])
 def column(matrix, column_number):
+    c = matrix.split('n')
+    z = int(column_number)
+    a = c[z]
+    column = []
+    for i in range(len(c)):
+        for j in range(len(c[i])):
+            R = (c[i][z])
+        column.append(R)
+    column = ' '.join(column)
+    print(column)
     # TODO: return column based on given column number
-    pass
+    return str(column)
 
 
-@app.route("/matrix/<matrix>/<row_number>", methods=['GET'])
+@app.route("/rows/matrix/<matrix>/<row_number>", methods=['GET'])
 def row(matrix, row_number):
+    b = matrix.split('n')
+    z = int(row_number)
+    a = b[z]
+    a = ' '.join(a)
+    print("a = " + a)
+    print(type(a))
     # TODO: return row based on given row number
-    pass
+    return a
 
 
 def pretty_json(arg):
@@ -48,3 +67,4 @@ def pretty_json(arg):
 
 if __name__ == "__main__":
     app.run(port=5000)
+
